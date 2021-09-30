043ce3378bc4a119138277cc255e40a6214691eb
User <User@DANIS-LENOVO>
2021-09-30 12:24:31 +0200

    Add project files.

---
+++ RecogniseNumbersBig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecogniseNumbersBig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
